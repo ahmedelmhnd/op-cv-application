9d73619c47b5a26ae3b810a5d19593bc94d85a9a
ahmed <ahmed.elmohanned@gmail.com>
2025-01-13 22:58:24 +0200

    added input form for general info, and handleSubmit function

--- src/components/InfoHeader.jsx
+++ src/components/InfoHeader.jsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
 
+function InfoHeader({ nameHandler }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    nameHandler(e.target.name.value);
+  }
 
-function InfoHeader({nameHandler}) {
+  return (
+    <div>
+      <h1>General Info</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" />
 
-    function handleName(e) {
-        nameHandler(e.target.value);
-    }
-
-  return <div>
-    <h1>General Info</h1>
-    <label htmlFor="name">Name</label>
-    <input type="text" id="name" onChange={handleName} />
-
-  </div>;
+        <button type="submit">
+          Save
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default InfoHeader;
