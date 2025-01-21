37e92c7da01b5c4c42948f0dfcaab5e063e18832
ahmed <ahmed.elmohanned@gmail.com>
2025-01-21 02:38:01 +0200

    added edit feature for each form with state, minor refactoring to allow

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,7 +1,13 @@
 import "../styles/Education.css";
+import { useState } from "react";
+import edit from "../assets/edit.png"
 
-function Education({ formHandler }) {
+function Education({ formHandler, info }) {
+
+  const [openStatus, setOpenStatus] = useState(false);
+
   function handleSubmit(e) {
+    setOpenStatus(false);
     e.preventDefault();
     const formData = {
       degree: e.target.degree.value,
@@ -16,56 +22,74 @@ function Education({ formHandler }) {
     formHandler(formData);
   }
 
-  return (
-    <div className="education">
-      <h1>Education</h1>
-      <form className="education-form" action="" onSubmit={handleSubmit}>
-        <div className="input-container">
-          <label htmlFor="degree">Degree:</label>
-          <select id="degree" name="degrees">
-            <option value="bachelor">Bachelor's Degree</option>
-            <option value="masters">Master's Degree</option>
-            <option value="doctorate">Doctorate</option>
-            <option value="diploma">Diploma</option>
-          </select>
-        </div>
+  function handleEdit(){
+    setOpenStatus(true);
 
-        <div className="input-container">
-          <label htmlFor="school">School / University:</label>
-          <input type="text" id="school" />
-        </div>
+  }
 
-        <div className="input-container">
-          <label htmlFor="course">Course:</label>
-          <input type="text" id="course" />
+    if (openStatus === true) {
+      return (
+        <div className="education">
+          <h1>Education</h1>
+          <form className="education-form" action="" onSubmit={handleSubmit}>
+            <div className="input-container">
+              <label htmlFor="degree">Degree:</label>
+              <select id="degree" name="degrees" defaultValue={info.degree}>
+                <option value="Bachelor's Degree">Bachelor's Degree</option>
+                <option value="Master's Degree">Master's Degree</option>
+                <option value="Doctorate">Doctorate</option>
+                <option value="Diploma">Diploma</option>
+              </select>
+            </div>
+    
+            <div className="input-container">
+              <label htmlFor="school">School / University:</label>
+              <input type="text" id="school" defaultValue={info.school} />
+            </div>
+    
+            <div className="input-container">
+              <label htmlFor="course">Course:</label>
+              <input type="text" id="course" defaultValue={info.course} />
+            </div>
+    
+            <div className="input-container">
+              <label htmlFor="grade">GPA / WAM:</label>
+              <input type="text" id="grade" defaultValue={info.grade}/>
+            </div>
+    
+            <div className="input-container">
+              <label htmlFor="from">Start-Date:</label>
+              <input type="date" id="from" defaultValue={info.from}/>
+            </div>
+    
+            <div className="input-container">
+              <label htmlFor="to">Projected / End-Date:</label>
+              <input type="date" id="to" defaultValue={info.to}/>
+            </div>
+    
+            <div className="about-container">
+              <label htmlFor="about">About:</label>
+              <textarea rows="2" cols="40" type="text" id="about" defaultValue={info.about}></textarea>
+            </div>
+    
+            <button className="submit-form-button" type="submit">
+              Save
+            </button>
+          </form>
         </div>
-
-        <div className="input-container">
-          <label htmlFor="grade">GPA / WAM:</label>
-          <input type="text" id="grade" />
+      );
+    } else {
+      return (
+        <div className="education">
+          <h1>Education</h1>
+          <div className="education-form">
+            <img className="edit-button" src={edit} alt="" onClick={handleEdit} />
+          </div>
         </div>
+      );
+    }
 
-        <div className="input-container">
-          <label htmlFor="from">Start-Date:</label>
-          <input type="date" id="from" />
-        </div>
 
-        <div className="input-container">
-          <label htmlFor="to">Projected / End-Date:</label>
-          <input type="date" id="to" />
-        </div>
-
-        <div className="about-container">
-          <label htmlFor="about">About:</label>
-          <textarea rows="2" cols="40" type="text" id="about"></textarea>
-        </div>
-
-        <button className="submit-form-button" type="submit">
-          Save
-        </button>
-      </form>
-    </div>
-  );
 }
 
 export default Education;
--- src/components/InfoHeader.jsx
+++ src/components/InfoHeader.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
-import "../styles/InfoHeader.css"
+import "../styles/InfoHeader.css";
+import edit from "../assets/edit.png";
 
-function InfoHeader({ formHandler }) {
+function InfoHeader({ formHandler, info }) {
+  const [openStatus, setOpenStatus] = useState(false);
 
   function handleSubmit(e) {
+    setOpenStatus(false);
     e.preventDefault();
     const formData = {
       firstName: e.target.firstName.value,
@@ -11,50 +14,70 @@ function InfoHeader({ formHandler }) {
       title: e.target.title.value,
       phone: e.target.phone.value,
       email: e.target.email.value,
-      about: e.target.about.value
+      about: e.target.about.value,
     };
 
     formHandler(formData);
   }
 
-  return (
-    <div className="general-info">
-      <h1>General Info</h1>
-      <form className="general-info-form" action="" onSubmit={handleSubmit}>
-        <div className="input-container">
-        <label htmlFor="firstName">First name:</label>
-        <input type="text" id="firstName" />
-        </div>
+  function handleEdit(){
+    setOpenStatus(true);
 
-        <div className="input-container">
-        <label htmlFor="lastName">Last name:</label>
-        <input type="text" id="lastName" />
-        </div>
+  }
 
-        <div className="input-container">
-        <label htmlFor="title">Title:</label>
-        <input type="text" id="title" />
-        </div>
+  if (openStatus === true) {
+    return (
+      <div className="general-info">
+        <h1>General Info</h1>
+        <form className="general-info-form" action="" onSubmit={handleSubmit}>
+          <div className="input-container">
+            <label htmlFor="firstName">First name:</label>
+            <input type="text" id="firstName" defaultValue={info.firstName}/>
+          </div>
 
-        <div className="input-container">
-        <label htmlFor="phone">Phone:</label>
-        <input type="tel" id="phone" />
-        </div>
+          <div className="input-container">
+            <label htmlFor="lastName">Last name:</label>
+            <input type="text" id="lastName" defaultValue={info.lastName} />
+          </div>
 
-        <div className="input-container">
-        <label htmlFor="email">E-mail:</label>
-        <input type="email" id="email" />
-        </div>
+          <div className="input-container">
+            <label htmlFor="title">Title:</label>
+            <input type="text" id="title" defaultValue={info.title}/>
+          </div>
 
-        <div className="about-container">
-        <label htmlFor="about">About:</label>
-        <textarea rows="4" cols="50" type="text" id="about" ></textarea>
-        </div>
+          <div className="input-container">
+            <label htmlFor="phone">Phone:</label>
+            <input type="tel" id="phone" defaultValue={info.phone}/>
+          </div>
 
-        <button className="submit-form-button" type="submit">Save</button>
-      </form>
-    </div>
-  );
+          <div className="input-container">
+            <label htmlFor="email">E-mail:</label>
+            <input type="email" id="email" defaultValue={info.email}/>
+          </div>
+
+          <div className="about-container">
+            <label htmlFor="about">About:</label>
+            <textarea rows="4" cols="50" type="text" id="about" defaultValue={info.about}></textarea>
+          </div>
+
+          <div className="buttons-container">
+            <button className="submit-form-button" type="submit">
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <div className="general-info">
+        <h1>General Info</h1>
+        <div className="general-info-form">
+          <img className="edit-button" src={edit} alt="" onClick={handleEdit} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default InfoHeader;
